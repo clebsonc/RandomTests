6ffc9bc1d5545eb33814a4364849be0259bf3de4
clebsonc <clebsondm@gmail.com>
2016-05-21 15:20:41 -0300

    One more test

--- test_git_hub_local.cpp
+++ test_git_hub_local.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 int main(){
-    std::cout << "Hello World Git from local to remote.";
-    
+    std::cout << "Hello World Git from local to remote." << std::endl;
+    std::cout << "Let's see the difference!" << std::endl;
     return 0;
 }

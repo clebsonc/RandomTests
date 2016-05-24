d7e52ea0531f6ce4b8a71ccc95b5f25a4503b764
clebsonc <clebsondm@gmail.com>
2016-05-24 02:41:34 -0300

    Data from the online repo, some random tests

---
+++ test_git_hub_local.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+    std::cout << "Hello World Git from local to remote." << std::endl;
+    std::cout << "Let's see the difference!" << std::endl;
+    return 0;
+}
